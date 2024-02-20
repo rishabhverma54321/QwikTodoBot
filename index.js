69926938b5485513e90fb88aeea82738b3e3a352
rishabhverma54321 <rishabhverma54321@gmail.com>
2024-02-21 01:09:28 +0530

    feat: app name change

---
+++ index.js
@@ -0,0 +1,23 @@
+import {Telegraf} from 'telegraf'
+import 'dotenv/config'
+
+const bot = new Telegraf(process.env.Token);
+
+
+bot.start((ctx)=>{
+    // console.log(ctx)
+   const name = ctx.from?.first_name;
+   ctx.reply(`Hi ${name}, this bot is at your service 🙇`)
+})
+
+bot.command('*', (ctx)=>{
+    console.log("hello rishbh")
+    ctx.reply('This command is not valid 😔' )
+})
+
+bot.use((ctx, next)=>{
+    // ctx.reply("how are you!")
+    ctx.reply('This command is not valid 😔' )
+})
+
+bot.launch()
